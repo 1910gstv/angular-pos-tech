0f95bd88e6f9bf3b178f397ef45fe2ddb39f8fc1
1910gstv <gustavo.hpaula@outlook.com>
2026-03-12 21:01:16 -0300

    feat: novo rota de upload

--- src/app/services/upload.ts
+++ src/app/services/upload.ts
@@ -1,32 +1,26 @@
 import { Injectable, inject } from '@angular/core';
-import {
-  HttpClient,
-  HttpEvent,
-  HttpHeaders
-} from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth-service';
 
 @Injectable({ providedIn: 'root' })
 export class UploadService {
-
   private http = inject(HttpClient);
-  private authService = inject(AuthService)
+  private authService = inject(AuthService);
 
   uploadVideo(file: File): Observable<HttpEvent<any>> {
-
-    const userData = this.authService.getUserInfo()
+    const userData = this.authService.getUserInfo();
 
     const formData = new FormData();
+    formData.append('userId', userData['userId']);
     formData.append('file', file);
 
     const headers = new HttpHeaders({
-      'Ocp-Apim-Subscription-Key': '94c8c9593701408fa7b18106dda8a3f5',
-      'userId': userData['userId']
+      'Ocp-Apim-Subscription-Key': '94c8c9593701408fa7b18106dda8a3f5'
     });
 
     return this.http.post(
-      'https://video-hackathon-fiap.azure-api.net/upload',
+      'https://video-hackathon-fiap.azure-api.net/v1/video/v1/video-processors/upload',
       formData,
       {
         headers,
@@ -34,6 +28,5 @@ export class UploadService {
         observe: 'events'
       }
     );
-
   }
 }
